2c05cb2cc128adaf13a324d30156dfc9e443cfeb
Faaiz@Pi <fr33ziey@gmail.com>
2020-04-12 23:24:22 +0100

    EDITED: Port numbers for mixing_tank

--- plants/implementation/launch_mixing_tank.py
+++ plants/implementation/launch_mixing_tank.py
@@ -8,13 +8,13 @@ from time import sleep
 from gpiozero import Button
 from gpiozero import LED
 
-from src.mixing_tank.mixing_plant import MixingTank
+from src.mixing_tank.mixing_tank import MixingTank
 from src.utils.util_functions import binary_stream_prep
 
 
 #   I   N   I   T   I   A   L   I   Z   A   T   I   O   N
 # Sample time
-h= 5
+h= 3
 
 #   C   O   N   F   I   G   U   R   A   T   I   O   N
 
@@ -22,7 +22,7 @@ h= 5
 # Configure Valve 1 on GPIO 21
 V1_input= Button(21)
 # Configure Valve 3 on GPIO 26
-V3_input= Button(26)
+V3_input= Button(12)
 # Configure Mixer on GPIO 20
 M_input= Button(20)
 
@@ -51,7 +51,7 @@ mp_1= LED(6)
 # Bit 2
 mp_2= LED(13)
 # Bit 3
-mp_3= LED(12)
+mp_3= LED(26)
 
 
 # Initialize Plant
@@ -64,21 +64,28 @@ while True:
     # V1
     if V1_input.is_pressed:
         V1= 1
-        sys.stdout.write("{0:10}: {1:5}".format("V1", bool(V1)))
+    else:
+        V1=0
+    sys.stdout.write("{0:10}: {1:5} \n".format("V1", bool(V1)))
     # V3
     if V3_input.is_pressed:
         V3= 1
-        sys.stdout.write("{0:10}: {1:5}".format("V3", bool(V3)))
+    else:
+        V3= 0
+    sys.stdout.write("{0:10}: {1:5} \n".format("V3", bool(V3)))
+        
     # M
     if M_input.is_pressed:
         M= 1
-        sys.stdout.write("{0:10}: {1:5}".format("Mixer", bool(M)))
+    else:
+        M= 0
+    sys.stdout.write("{0:10}: {1:5} \n".format("Mixer", bool(M)))
 
     mixing_tank.update(V1=V1, V3=V3, M=M)
 
     # Set outputs
     # ls_1
-    if mixing_tank.ls_1:
+    if not mixing_tank.ls_1:
         ls_1_led.on()
     else:
         ls_1_led.off()
@@ -90,15 +97,15 @@ while True:
         ls_2_led.off()
 
     # Tank liquid level
-    ll= binary_stream_prep(mixing_tank.t_l_l)
-    ll_0, ll_1, ll_2, ll_3= ll
+    # ll= binary_stream_prep(mixing_tank.t_l_l)
+    # ll_0, ll_1, ll_2, ll_3= ll
 
     # Tank mix percentage
-    mp= binary_stream_prep(mixing_tank.m_p)
-    mp_0, mp_1, mp_2, mp_3= mp
+    # mp= binary_stream_prep(mixing_tank.m_p)
+    # mp_0, mp_1, mp_2, mp_3= mp
 
     # Print state to console
-    self.print()
+    mixing_tank.print()
 
     #Set operatoin period to sample time
     sleep(h)
